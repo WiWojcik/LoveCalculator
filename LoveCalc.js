51a0e8635ae2a29cb2bda2cb37812b7af2ec5d4c
WiWojcik <155015429+WiWojcik@users.noreply.github.com>
2024-02-02 12:52:25 +0100

    Update LoveCalc.js

--- LoveCalc.js
+++ LoveCalc.js
@@ -1,10 +1,17 @@
-
 function calc(){ 
    var rndNum = (Math.random() * 100) + 1;
     return Math.floor(rndNum)
 }
 prompt("Write your name: ")
 prompt("Now write name of your crush: ")
-alert("Match chance is " + calc() + "%")
-//console.log(calc())
-
+var lovenum = calc();
+ 
+if(lovenum > 70){
+    alert("Match chance is " + lovenum + "% " + "GOOOD you love so hard")
+}
+if(lovenum > 30 && lovenum <=70){
+alert("Match chance is " + lovenum + "%")
+}
+if (lovenum <=30){
+   alert("Match chance is " + lovenum + "%" + " you die alone ") 
+}
