e929e6b32b417e33a6ca2910e3e0109c99d0db27
WiWojcik <155015429+WiWojcik@users.noreply.github.com>
2024-02-02 11:50:46 +0100

    Create LoveCalc.js

---
+++ LoveCalc.js
@@ -0,0 +1,10 @@
+
+function calc(){ 
+   var rndNum = (Math.random() * 100) + 1;
+    return Math.floor(rndNum)
+}
+prompt("Write your name: ")
+prompt("Now write name of your crush: ")
+alert("Match chance is " + calc() + "%")
+//console.log(calc())
+
